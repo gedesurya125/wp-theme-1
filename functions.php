e62412a6ca42f5b71c4b7b3e27138e2b02a07497
gedesurya125 <gedesurya125@gmail.com>
2024-10-23 00:27:02 +0800

    add custom fonts

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
 
 function gs_load_script(){
-  wp_enqueue_style('gs-style', get_stylesheet_uri(),  array(), filemtime(get_template_directory(  ).'/style.css'), 'all');
+  wp_enqueue_style(
+    'gs-style', 
+    get_stylesheet_uri(),  
+    array(), 
+    filemtime(get_template_directory(  ).'/style.css'), //? enable only in the development mode. otherwise use the css version number instead
+    'all'
+  );
+
+  wp_enqueue_style( 'gs_google_font_poppies', get_template_directory_uri().'/assets/fonts/Poppins/font.css', array() );
+  
 };
 
 add_action( 'wp_enqueue_scripts', 'gs_load_script' );
---
+++ readme.md
@@ -0,0 +1,4 @@
+## reference:
+
+- How to host google fonts locally on the wordpress: https://wplift.com/how-to-host-google-fonts-locally-on-wordpress/
+- Font converter website https://convertio.co/ttf-woff/
