78922756d43eb7a4210d8d8f528736a148844a64
gedesurya125 <gedesurya125@gmail.com>
2024-11-17 15:29:01 +0800

    add the menu depth limit to the footer

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 <footer>
   <?php wp_footer(  ) ?>
   <nav>
-    <?php wp_nav_menu( )?>
+    <!-- the depth parameter, limit the depth level of the menu. Note 0 mean unlimited -->
+    <?php wp_nav_menu(array("location"=> "wp_devs_footer_menu", "depth" => 2) )?> 
   </nav>
 </footer>
 </html>
